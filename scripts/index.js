272ab8c1f06eb4361a42c0cd7537d502a1f46a61
Tanya <tanya.pavlova19@yandex.ru>
2021-09-16 19:52:19 +0300

    review 3

--- scripts/index.js
+++ scripts/index.js
@@ -3,19 +3,18 @@ const popupOpenBtn = document.querySelector('.profile-info__edit-button')
 const popupCloseBtn = popup.querySelector('.popup__button-close')
 
 let formElement = document.querySelector('.popup__form')
-let nameInput = formElement.querySelector('.popup__field_name')
-let jobInput = formElement.querySelector('.popup__field_descripton')
+let nameInput = formElement.querySelector('.popup__field_name_title')
+let jobInput = formElement.querySelector('.popup__field_name_description')
 
 let profileName = document.querySelector('.profile-info__name')
 let profileJob = document.querySelector('.profile-info__description')
-let popupSave = popup.querySelector('.popup__button-save')
 
 
 
 function popupToggle () {
     popup.classList.toggle('popup_open');
-    nameInput.textContent = profileName.value;
-    jobInput.textContent = profileJob.value;
+    nameInput.value = profileName.textContent
+    jobInput.value = profileJob.textContent
 }
 
 // Обработчик «отправки» формы, хотя пока
@@ -28,16 +27,17 @@ function formSubmitHandler (evt) {
 // Вставьте новые значения с помощью textContent
      profileName.textContent = nameInput.value;
      profileJob.textContent = jobInput.value;
-}
 
-function popupClose () {
-     popup.classList.toggle('popup')
+// Закрытие попап после сохранения данных
+     function popupClose () { 
+          popup.classList.toggle('popup') 
+     }
+     
+     popupClose()
 }
 
 // // Прикрепляем обработчик к форме:
 // // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
-
 popupOpenBtn.addEventListener('click', popupToggle)
 popupCloseBtn.addEventListener('click', popupToggle)
-popupSave.addEventListener('click', popupClose);
