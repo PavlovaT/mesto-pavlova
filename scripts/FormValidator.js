476d33c5702ea8dc4a6745007feb64f1828f425e
Tanya Pavlova <tanya@Tanyas-Air.localdomain>
2021-11-04 22:23:54 +0300

    review 1

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -1,75 +1,77 @@
 export default class FormValidator {
-    // constructor (data, form) {
-    //     
-    // }
-}
+    constructor (data, form) {
+        this._form = form;
+        this._inputSelector = data.inputSelector;
+        this._submitButtonSelector = data.submitButtonSelector;
+        this._inactiveButtonClass =  data.inactiveButtonClass;
+        this._inputErrorClass = data.inputErrorClass;
+        this._errorClass = data.errorClass;
+    }
 
+    _isValidForm() {
+        const inputList = Array.from(this._form.querySelectorAll(this._inputSelector))
+        return !inputList.some(inputElement => {
+            return !inputElement.validity.valid
+        })
+    }
 
-
-
-// const toggleButtonState = (button, isActive, config) => {
-//     if(isActive) {
-//         button.classList.remove(config.inactiveButtonClass);
-//         button.disabled = false;
-//         console.log('active')
-//     } else {
-//         button.classList.add(config.inactiveButtonClass);
-//         button.disabled = 'disabled';
-//         console.log('not active')
-//     }
-// }
-
-// function showError(errorElement, inputElement, config) {
-//     errorElement.textContent = inputElement.validationMessage
-//     inputElement.classList.add(config.inputErrorClass)
-// }
-
-// function hideError(errorElement, inputElement, config) {
-//     errorElement.textContent = ''
-//     inputElement.classList.remove(config.inputErrorClass)
-// }
-
-// const checkInputValidity = (formElement, inputElement, config) => {
-//     const isInputNotValid = !inputElement.validity.valid
-//     const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
-//     if(isInputNotValid) {
-//         showError(errorElement, inputElement, config)
-//     } else {
-//         hideError(errorElement, inputElement, config)
-//     }
-// }
-
-// const setEventListers = (formElement, config) => {
-//     const inputList = formElement.querySelectorAll(config.inputSelector)
-//     const submitButton = formElement.querySelector(config.submitButtonSelector)
-
-//     Array.from(inputList).forEach(inputElement => {
-//         inputElement.addEventListener('input', (evt) => {
-//             checkInputValidity(formElement, inputElement, config)
-//             toggleButtonState(submitButton, formElement.checkValidity(), config)
-//         })
-//     })
-
-//     formElement.addEventListener('submit', (evt) => {
-//         evt.preventDefault()
-//     })
-// }
-
-// const enableValidation = (config) => {
-//     const forms = document.querySelectorAll(config.formSelector)
+    _showError(errorElement, inputElement) {
+        errorElement.textContent = inputElement.validationMessage;
+        inputElement.classList.add(this._inputErrorClass);
+    }
     
-//     Array.from(forms).forEach(formElement => {
-//         setEventListers(formElement, config)
-//     })
-// }
+    _hideError(errorElement, inputElement) {
+        errorElement.textContent = ''
+        inputElement.classList.remove(this._inputErrorClass)
+    }
 
-// const validationConfig = {
-//   formSelector: '.popup__form',
-//   inputSelector: '.popup__field',
-//   submitButtonSelector: '.popup__button-save',
-//   inactiveButtonClass: 'popup__button-save_type_block',
-//   inputErrorClass: 'popup__field_state_invalid',
-//   errorClass: 'error'
-// }
+    //активная/заблокированная кнопка
+    _toggleButtonState(button){
+        if(this._isValidForm()) {
+            button.classList.remove(this._inactiveButtonClass);
+            button.disabled = false;
+            console.log('active')
+        } else {
+            button.classList.add(this._inactiveButtonClass);
+            button.disabled = true;
+            console.log('not active')
+        }
+    }
 
-// enableValidation(validationConfig)
+    //добавление блокированной кнопки
+    blockButton(button) {
+        button.classList.add(this._inactiveButtonClass)
+        button.disabled = true;
+    }
+
+    _checkInputValidity (inputElement) {
+        const isInputNotValid = !inputElement.validity.valid
+        const errorElement = this._form.querySelector(`#${inputElement.id}-error`); 
+
+        if (isInputNotValid) {
+          this._showError(errorElement, inputElement);
+        } else {
+          this._hideError(errorElement, inputElement);
+        }
+    }
+
+    _setEventListeners() {
+        const inputList = this._form.querySelectorAll(this._inputSelector);
+        const submitButton = this._form.querySelector(this._submitButtonSelector);
+
+        Array.from(inputList).forEach(inputElement => {
+            inputElement.addEventListener('input', () => {
+                this._checkInputValidity(inputElement);
+                this._toggleButtonState(submitButton);
+            });
+        });
+
+        this._form.addEventListener('submit', (evt) => { 
+            evt.preventDefault()
+        })
+    }
+
+    enableValidation() {
+        this._setEventListeners();
+    }
+}
--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
-import Card from './Card.js'
+import Card from './card.js'
+import FormValidator from './formValidator.js'
 
 const initialCards = [
   {
@@ -41,14 +42,24 @@ const popupProfile = document.querySelector('.popup__form_profile')
 const imagePopup = document.querySelector('.popup_type_image')
 const popupCard = document.querySelector('.popup__form_card')
 const placeArticle = document.querySelector('.elements')
+const popupCardName = document.querySelector('.popup__field_name_heading')
+const popupCardLink = document.querySelector('.popup__field_name_link')
 
-//лишнее
-// initialCards.forEach(data => {
-//   const cardElement = new Card(data, '#element-template').render();
-  
-//   placeArticle.append(cardElement);
-//   console.log('work')
-// });
+
+const validationConfig = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__field',
+  submitButtonSelector: '.popup__button-save',
+  inactiveButtonClass: 'popup__button-save_type_block',
+  inputErrorClass: 'popup__field_state_invalid',
+  errorClass: 'error'
+}
+
+const validatorProfilePopup = new FormValidator(validationConfig, profilePopup);
+const validatorElmPopup= new FormValidator(validationConfig, elmPopup);
+
+  validatorProfilePopup.enableValidation();
+  validatorElmPopup.enableValidation();
 
 //открытие оставляю
 function openPopup(modal) {
@@ -67,10 +78,11 @@ root.removeEventListener('keydown', keyHandler)
 
 //закрытие попапа через крестик  оставляю
 function closePopupBtn(evt) {
-const closeBtn = evt.target;
-if (closeBtn.classList.contains('popup__button-close')) {
-  closePopup ()
-}
+  const closeBtn = evt.target;
+  if (closeBtn.classList.contains('popup__button-close')) {
+    popupCard.reset();
+    closePopup ()
+  }
 }
 
 //закрытие попапа через оверлей  оставляю
@@ -101,43 +113,45 @@ function handlerProfileSubmit(evt) {
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
 
-  closePopup(evt)
+  closePopup()
 }
 
-//создание карточки в попапе "создать"  ВОТ ЭТА ФУНКЦИЯ 
+//создание карточки в попапе "создать" 
 function createProfileCards(data) {
   const newCard = new Card(data.name, data.link, '#element-template').render()
-  console.log(newCard)
+ 
   return newCard
 }
 
-//перебор массива  ВОТ ТУТ
+//перебор массива
 const renderElements = () => {
   initialCards.forEach((item)=> {
     placeArticle.append(createProfileCards(item));
   })
-  console.log(123)
 }
 
 renderElements()
 
-//ВОТ ЗДЕСЬ СЛУШАТЕЛЬ
 popupCard.addEventListener('submit', function (evt) {
   evt.preventDefault()
-  createProfileCards()
-  console.log(50)
+  const data = {
+    name: popupCardName.value,
+    link: popupCardLink.value
+  }
+  placeArticle.prepend(createProfileCards(data))
+  evt.currentTarget.reset()
+  closePopup()
 })
 
 root.addEventListener('click', closePopupBtn)
 profilePopup.addEventListener('click', closeClickOverlay)
 elmPopup.addEventListener('click', closeClickOverlay)
 imagePopup.addEventListener('click', closeClickOverlay)
-profilePopupBtn.addEventListener('click', openProfilePopup)
 popupProfile.addEventListener('submit', handlerProfileSubmit)
+profilePopupBtn.addEventListener('click', openProfilePopup)
 
 editPopupBtn.addEventListener('click', () => {
+  const submitButton = elmPopup.querySelector('.popup__button-save')
   openPopup(elmPopup)
-
-  // const submitButton = elmPopup.querySelector('.popup__button-save')
-  // toggleButtonState(submitButton, false, validationConfig)
+  validatorElmPopup.blockButton(submitButton)
 })
